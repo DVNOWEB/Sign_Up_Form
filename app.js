8bf0223bdc0bbf385f5f68923d0552c4b8185b0b
DVNO <dvno@live.se>
2022-12-07 12:13:25 +0100

    🚀 style

---
+++ app.js
@@ -0,0 +1,6 @@
+const container = document.querySelector('.container')
+const signUpBtn = document.querySelector('.blue-bg')
+
+signUpBtn.addEventListener('click', () => {
+   container.classList.toggle('change')
+})
